9d52aff5342b8ddcd54975deb3e9b0ba4574fbec
ManjushaJ <82754663+Mjp777@users.noreply.github.com>
2024-05-06 20:48:08 +0530

    Update string token.cpp

--- string token.cpp
+++ string token.cpp
@@ -3,15 +3,21 @@ import java.util.*;
 
 public class Solution {
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        String s = scan.nextLine().trim();
-        scan.close();
-        String[] tokens=s.split("[ !,?._'@]+");
-        System.out.println(tokens.length);
-        for(String token:tokens)
-        {
-            System.out.println(token);
-        }
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    String s = scan.nextLine();
+
+    String[] result = s.trim().split("[ !,?._'@]+");
+
+    if (s.trim().equals("")) {
+      System.out.println("0");
+    } else {
+      System.out.println(result.length);
     }
+    for (String aResult : result) {
+      System.out.println(aResult);
+    }
+
+    scan.close();
+  }
 }
