15a73e00cb740f2f8a68ca82791c966f1ce80432
ManjushaJ <82754663+Mjp777@users.noreply.github.com>
2024-06-04 10:59:32 +0530

    Create sum vs XOR.java

---
+++ sum vs XOR.java
@@ -0,0 +1,22 @@
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        long n = in.nextLong();
+        long count = 0;
+        //This performs a basic conversion from int to binary using divide by two and checking even or odd
+        while(n != 0){
+            count += (n%2 == 0)?1:0;
+            n/=2; 
+        }
+        count = (long) Math.pow(2,count);
+        System.out.println(count);
+    }
+}
